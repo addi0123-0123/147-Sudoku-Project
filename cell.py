d8fdd745a00b310e1cdc51d1421dc148df739090
addi0123-0123 <addi0123@pm.me>
2025-11-26 17:20:56 -0500

    This file defines Cell class.
    draw method is completed.

---
+++ cell.py
@@ -0,0 +1,25 @@
+import pygame, sys
+from constants import *
+
+class Cell:
+    def __init__(self, value, row, col):
+        self.value = value # numbers 1-9 or '0'
+        self.row = row
+        self.col = col
+
+    def set_cell_value(self, value):
+        pass
+
+    def set_sketched_value(self, value):
+        pass
+
+    def draw(self, screen):
+        num_font = pygame.font.Font(None, NUM_FONT)
+
+        if self.value == '0': # leave cells w/ value '0' empty on screen.
+            return
+
+        text_surf = num_font.render(str(self.value), True, PRINTED_NUM_COLOR)
+        text_rect = text_surf.get_rect(center=(self.col * SQUARE_SIZE + SQUARE_SIZE / 2,
+                                               self.row * SQUARE_SIZE + SQUARE_SIZE / 2))
+        screen.blit(text_surf, text_rect)
